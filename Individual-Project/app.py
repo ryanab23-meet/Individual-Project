9ddfea4423d53627498b9d004baa0088b0aabbcb
rryannnn111 <94308251+rryannnn111@users.noreply.github.com>
2022-08-07 22:31:59 +0300

    s

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -1,21 +1,48 @@
-from flask import Flask, render_template, request
-from database import *
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import session as login_session
+import pyrebase
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'changeme'
+Config = {
+
+"iKey":"AIzaSyC9p3c98SMicj7eGggCmgBlOISHfEelHLY",
+
+"authDomain": "project-1-86a2e.firebaseapp.com",
+
+"projectId": "project-1-86a2e",
+
+"storageBucket": "project-1-86a2e.appspot.com",
+
+"messagingSenderId": "495478794645",
+
+"appId":project-1-495478794645-web-9efdaebf683dde422cae9c",
+
+"measurementId": "G-99674BFEP4",
+
+ "databaseURL": "https://project1-20262-default-rtdb.firebaseio.com"
+
+}
+
+firebase = pyrebase.initialize_app(Config)
+auth = firebase.auth()
+db = firebase.database()
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-    email = request.form['email']
-    
-    
-@app.route('/signup', methods=['GET', 'POST'])
+       return render_template("index.html")
+       email = request.form['email']
+       password = request.form['password']
+
+
+@app.route('/signup',methods=['GET', 'POST']):
 def signup():
-        return render_template('signup.html')
-    else:
-        return render_template('login.html')
+if request.method == 'POST':
+email = request.form['email']
+password =request.form['password']
+print(email, password)
+# try:
+login_session['user'] = auth.create_user_with_email_and_password(email, password)
+user = {"name": request.form['name'], "email" : email}
 
 
 @app.route("/comedy.html")
@@ -24,7 +51,7 @@ def comedy():
 
 
 
-@app.route("/action.html")
+@aupp.route("/action.html")
 def action():
     return render_template("action.html")  
 
--- Individual-Project/static/css/script.js
+++ Individual-Project/static/css/script.js
@@ -1,14 +1,17 @@
-    var el_up = document.getElementById("GFG_UP");
-            var el_down = document.getElementById("GFG_DOWN");
-            var str = "Click on button to change the background color";
+var el_up = document.getElementById("GFG_UP");
+var el_down = document.getElementById("GFG_DOWN");
+var str = "Click on button to change the background color";
+ 
+el_up.innerHTML = str;
           
-            el_up.innerHTML = str;
-          
-            function changeColor(color) {
-                document.body.style.background = color;
-            }
+function changeColor(color) {
+document.body.style.background = color;
+
+}
               
-            function gfg_Run() {
-                changeColor('grey');
-                el_down.innerHTML = "Background Color succesfully changed";
-            }         
+            
+function gfg_Run() {
+changeColor('grey');
+el_down.innerHTML = "Background Color succesfully changed";
+
+}
